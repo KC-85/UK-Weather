717b9c40f3538153db29663053598923b26d21d8
Kristian Cross <etamin1985@yahoo.com>
2026-07-30 18:25:08 +0100

    Add weather url

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("locations/", include("apps.locations.urls")),
+    path("weather/", include("apps.weather.urls")),
 ]
