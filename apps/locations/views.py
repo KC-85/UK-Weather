01e7f0084cd25d716e0a1efd1a94964a4abbf3b4
Kristian Cross <etamin1985@yahoo.com>
2026-07-30 15:51:04 +0100

    Add GeoJSON endpoint in views to return codes, names, polygons, filters

--- apps/locations/views.py
+++ apps/locations/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.core.serializers import serialize
+from django.http import HttpResponse
 
-from .forms import LocationSearchForm
+from .models import Region
 
 
-def search(request):
-    form = LocationSearchForm(request.GET)
-    context = {"form": form, "query": ""}
+def regions_geojson(request):
+    regions = Region.objects.all()
 
-    if form.is_valid():
-        context["query"] = form.cleaned_data["query"]
+    geojson = serialize(
+        "geojson",
+        regions,
+        geometry_field="boundary",
+        fields=("code", "name", "region_type"),
+    )
 
-    return render(request, "locations/partials/search_results.html", context)
+    return HttpResponse(
+        geojson,
+        content_type="application/geo+json",
+    )
