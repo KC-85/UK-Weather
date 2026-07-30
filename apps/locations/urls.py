75efcc94daad235e76be362c4aaedeb88cb3dc2c
Kristian Cross <etamin1985@yahoo.com>
2026-07-30 18:09:41 +0100

    Comment out search

--- apps/locations/urls.py
+++ apps/locations/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "locations"
 
 urlpatterns = [
-    path("search/", views.search, name="search"),
+    # path("search/", views.search, name="search"),
     path(
         "regions.geojson",
         views.regions_geojson,
