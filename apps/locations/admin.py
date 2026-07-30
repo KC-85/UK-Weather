80d02e4cf22b0525618eb6588288289e58c141d8
Kristian Cross <etamin1985@yahoo.com>
2026-07-30 14:13:20 +0100

    Register both models with admin.py

--- apps/locations/admin.py
+++ apps/locations/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
 
-from .models import SavedLocation
+from .models import Region, SavedLocation
 
-admin.site.register(SavedLocation)
+
+@admin.register(Region)
+class RegionAdmin(GISModelAdmin):
+    list_display = (
+        "name",
+        "code",
+        "region_type",
+        "parent",
+    )
+    list_filter = ("region_type",)
+    search_fields = ("name", "code")
+    autocomplete_fields = ("parent",)
+    ordering = ("name",)
+
+
+@admin.register(SavedLocation)
+class SavedLocationAdmin(GISModelAdmin):
+    list_display = (
+        "name",
+        "user",
+        "region",
+        "created_at",
+    )
+    search_fields = (
+        "name",
+        "user__username",
+        "region__name",
+    )
+    autocomplete_fields = ("user", "region")
+    list_select_related = ("user", "region")
+    readonly_fields = ("created_at",)
+    ordering = ("name",)
