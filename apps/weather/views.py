015d5ee1cc3d1a3a4d70d29b8e2738681558118c
Kristian Cross <etamin1985@yahoo.com>
2026-07-31 14:53:56 +0100

    Add current weather conditions endpoint

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -1,4 +1,48 @@
-from django.shortcuts import render
+import logging
+
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_GET
+
+from apps.locations.models import Region
+
+from .services.client import OpenMeteoClient, WeatherClient
+from .services.exceptions import WeatherServiceError
+from .services.transformers import normalize_current_conditions
+
+logger = logging.getLogger(__name__)
+
+
+@require_GET
+def current_conditions(request, code):
+    region = get_object_or_404(Region, code=code)
+    weather_client = WeatherClient(OpenMeteoClient())
+
+    try:
+        response = weather_client.forecast(
+            latitude=region.forecast_point.y,
+            longitude=region.forecast_point.x,
+        )
+        conditions = normalize_current_conditions(response)
+    except WeatherServiceError as error:
+        logger.warning(
+            "Unable to load weather for region %s: %s",
+            region.code,
+            error,
+        )
+        return render(
+            request,
+            "weather/partials/weather_error.html",
+            {"region": region},
+        )
+
+    return render(
+        request,
+        "weather/partials/current_conditions.html",
+        {
+            "conditions": conditions,
+            "region": region,
+        },
+    )
 
 
 def detail(request, location):
