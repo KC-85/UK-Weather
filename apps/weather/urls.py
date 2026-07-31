6b5efc75ceda2e6e709f10f499b14afe632a3795
Kristian Cross <etamin1985@yahoo.com>
2026-07-31 14:57:54 +0100

    Add url path to connect current conditions

--- apps/weather/urls.py
+++ apps/weather/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = "weather"
 
 urlpatterns = [
+    path(
+        "regions/<str:code>/current/",
+        views.current_conditions,
+        name="current_conditions",
+    ),
     path("<slug:location>/", views.detail, name="detail"),
 ]
